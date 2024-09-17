fe70ec3a08518c337c8a9de50e9f5a47e121e517
Bruno1571 <brunoconde29@gmail.com>
2024-09-16 23:58:45 -0300

    creacion de la vista create y su url, creacion de templates y configuracion de las url en el proyecto

--- post/views.py
+++ post/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Post
 
-# Create your views here.
+def posts_create(request):
+    if request.method == 'POST':
+        title = request.POST['Titulo']
+        content = request.POST['Contenido']
+        post = Post(Titulo=title, Contenido=content, Autor=request.user)
+        post.save()
+        return redirect('create.html')
+    return render(request, 'create.html')
+
+
