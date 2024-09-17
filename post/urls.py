86f2d8432fe3506fd2f642886b22ea01cf9d2dfc
Bruno1571 <brunoconde29@gmail.com>
2024-09-17 14:31:34 -0300

    configuracion de la url de posts_list

--- post/urls.py
+++ post/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('posts/create/', views.posts_create, name='crear_post'),
+    path('posts/', views.posts_list, name='lista_post'),
     # Otras URLs...
 ]
